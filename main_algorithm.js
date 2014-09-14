997a9c547965a626697dd90922b0ef1c916d4cb5
jacobstein123 <jacobstein123@gmail.com>
2014-09-14 05:40:35 -0400

    trying to get assignment rankings from each class

--- main_algorithm.js
+++ main_algorithm.js
@@ -11,6 +11,7 @@ function findBestDay(db, school, class_, type, startDate, endDate){
 
 var classes = db.collection("classes");
 var users = db.collection("user");
+var calendar = db.collection("calendar");
 var students;
 var difficultyList;
 classes.find({className:"judaics 101"},{"students":1, _id:0},function(err,docs){
@@ -21,7 +22,7 @@ classes.find({className:"judaics 101"},{"students":1, _id:0},function(err,docs){
 		console.log(docs[0].difficultyList);
 		difficultyList = docs[0].difficultyList;
 
-		getClasses = function(student,isLast){
+		getClasses = function(student,isLast,endCallBack){
 			if (!isLast){
 				users.find({"name":student},{"classes":1, _id:0},function(err,docs){
 					console.log(docs[0].classes);
@@ -32,11 +33,7 @@ classes.find({className:"judaics 101"},{"students":1, _id:0},function(err,docs){
 			}
 
 			else{
-				users.find({"name":student},{"classes":1, _id:0},function(err,docs){
-					console.log(docs[0].classes);
-					studentClasses.push(docs[0].classes);
-					console.log("THE FINAL LIST: "+studentClasses);
-				});
+				users.find({"name":student},{"classes":1, _id:0},endCallBack);
 			}
 			
 		};
@@ -47,11 +44,34 @@ classes.find({className:"judaics 101"},{"students":1, _id:0},function(err,docs){
 
 		console.log(studentClasses);
 		for (i=0; i<students.length-1; i++){
-			getClasses(students[i],false);
+			getClasses(students[i],false,addingUpAssignments);
 		}
-		getClasses(students[students.length-1],true);
-
 		
+		getAssignmentRanking = function(_class_,isLast){
+			if (!isLast){
+				calendar.find({},function(err,docs){
+					console.log("assignment: "+docs[0].assignment);
+					console.log("dff: "+difficultyList[docs[0].assignment]);
+					ranking += difficultyList[docs[0].assignment];
+					console.log("rank: "+ranking);
+				});
+			}
+			
+		};
+		var addingUpAssignments = function(err,docs){
+			console.log(docs[0].classes);
+			studentClasses.push(docs[0].classes);
+			console.log("THE FINAL LIST: "+studentClasses);
+
+			var ranking = 0;
+			for (i=0; i<studentClasses.length-1; i++){
+				getAssignmentRanking(studentClasses[i],false);
+			}
+
+			getAssignmentRanking(studentClasses[studentClasses.length-1],true);
+
+		};
+		getClasses(students[students.length-1],true,addingUpAssignments);
 	});
 });
 
